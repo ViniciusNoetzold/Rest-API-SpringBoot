3622df6d0d88ea1a75b21b404b8305bddbaf802f
ViniciusNoetzold <viniciusanoetzold@gmail.com>
2025-11-21 16:55:32 +0000

    .

--- API-example/src/main/java/br/edu/atitus/api_example/configs/WebConfig.java
+++ API-example/src/main/java/br/edu/atitus/api_example/configs/WebConfig.java
@@ -1,17 +1,8 @@
 package br.edu.atitus.api_example.configs;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 }
